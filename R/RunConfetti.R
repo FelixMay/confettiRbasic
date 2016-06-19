ca9f653c1b1cfa169f70fd4cac58429279b6f0c8
FelixMay <felix.may@idiv.de>
2016-06-19 22:05:09 +0200

    Update trade-offs and update documentation and vignettes

--- R/RunConfetti.R
+++ R/RunConfetti.R
@@ -1,6 +1,6 @@
 #' Run the CONFETTI model
 #'
-#' @param pars A vector of 22 numbers that include the model parameters for CONFETTI. The vector can be named but need not. Note that the order of parameters matters.
+#' @param pars Model parameters of CONFETTI. Either a vector of length 23 or a named vector of variable length. The names have to match the following list. When the parameter is unnamed the order has to correspond to the following list.
 #'
 #'\enumerate{
 #'    \item \bold{metaSR} - Species richness of the metacommunity
@@ -14,51 +14,42 @@
 #' \item \bold{CNDD.cv} - Coefficient of variation (sd/mean) of CNDD among species
 #' \item \bold{pRec.m} - mean recruitment probability without competition in interval [0.001; 1]
 #' \item \bold{pRec.cv} - Coefficient of variation (sd/mean) of recruitment probability among species
-#' \item \bold{trade1.CNDD.pRec} - mode for trade-off between CNDD and recruitment probability:
+#' \item \bold{trade1.CNDD.pRec} - mode for trade-off between CNDD and recruitment probability. There are three different possible modes (parameterizations) for each of the three trade-offs:
 #' \itemize {
 #'    \item 0: no trade-off
-#'    \item 1: linear trade-off with \eqn{CNDD = a + b * pRec}
-#'    \item 2: exponential trade-off with \eqn{CNDD = a + b * exp(c * pRec)}
+#'    \item 1: linear: \eqn{y = a + b * x}
+#'    \item 2: logarithmic: \eqn{y = a + b * log(x)}
+#'    \item 3: exponential: \eqn{y = a + b * exp(c * x)}
 #' }
-#' \item \bold{a.CNDD.pRec} - parameter for CNDD-recruitment trade-off
-#' \item \bold{b.CNDD.pRec} - parameter for CNDD-recruitment trade-off
-#' \item \bold{c.CNDD.pRec} - parameter for CNDD-recruitment trade-off
-#' \item \bold{trade2.CNDD.abund} - mode for trade-off between CNDD and metacomminuty relative abundance:
-#' \itemize {
-#'    \item 0: no trade-off
-#'    \item 1: linear trade-off with \eqn{CNDD = a + b * rel.abund}
-#'    \item 2: logarithmic trade-off with \eqn{CNDD = a + b * log(rel.abund)}
-#' }
-#' \item \bold{a.CNDD.abund} - parameter for CNDD-metacommunity abundance trade-off
-#' \item \bold{b.CNDD.abund} - parameter for CNDD-metacommunity abundance trade-off
+#' \item \bold{a.CNDD.pRec} - parameters for CNDD-recruitment trade-off
+#' \item \bold{b.CNDD.pRec}
+#' \item \bold{c.CNDD.pRec}
+#' \item \bold{trade2.CNDD.abund} - mode for trade-off between CNDD and metacommunity relative abundance:
+#' \item \bold{a.CNDD.abund} - parameters for CNDD-metacommunity abundance trade-off
+#' \item \bold{b.CNDD.abund}
+#' \item \bold{c.CNDD.abund}
 #' \item \bold{trade3.disp.pRec} - mode for trade-off between dispersal and recruitment probability:
-#' #' \itemize {
-#'    \item 0: no trade-off
-#'    \item 1: linear trade-off with \eqn{disp = a + b * pRec}
-#'    \item 2: exponential trade-off with \eqn{disp = a + b * exp(c * pRec)}
-#' }
-#' \item \bold{a.disp.pRec} - parameter for dispersal-recruitment trade-off
-#' \item \bold{b.disp.pRec} - parameter for dispersal-recruitment trade-off
-#' \item \bold{c.disp.pRec} - parameter for dispersal-recruitment trade-off
+#' \item \bold{a.disp.pRec} - parameters for dispersal-recruitment trade-off
+#' \item \bold{b.disp.pRec}
+#' \item \bold{c.disp.pRec}
 #' }
 #' @param nRep Number of replicate runs with the same parameter set
 #' @param nGen Number of generations simulated. This means the model is simulates nGen*nTrees birth-death events
-#' @param nSteps.out Number of time steps with model output. If nSteps.out == 1 there is only model output from the last simulated time step. If nSteps.out > 1 there is output over time. The output time itnerval is calculated as nGen/nSteps.out. The initial condition is included in the output. Temporal output works only on combination with avg = F.
+#' @param nSteps.out Number of time steps with model output. If nSteps.out == 1 there is only model output from the last simulated time step. If nSteps.out > 1 there is output over time. The output time interval is calculated as nGen/nSteps.out. The initial condition is included in the output. Temporal output works only on combination with avg = F.
 #' @param nTrees Number of trees in the local community
 #' @param Xext Size of the local community in x-direction
 #' @param Yext Size of the local community in y-direction
 #' @param dist.max Maximum distance (in meters) over which the spatial patterns F(r) and PCF(r) are calculated.
 #' @param bin.width The distance bin-width used for the calculation of the spatial patterns F(r) and PCF(r),
 #' @param meta.SAD The abundance distribution of the metacommunity: (0) uniform distribution, (1) log-normal distribution with coefficient of variation metaCV.
-#' @param avg logical variable: Should the non-temporal model output be averaged over the replicate runs? This works only if nSteps.out == 1.
+#' @param avg Logical variable: Should the non-temporal model output be averaged over the replicate runs? This works only if nSteps.out == 1.
 #'
-#' @return The model simulates virtual tree censuses with coordinates and species identities for each tree. The census data of the last simulated time step is only provided as output when nRep = 1 and avg = F. In this case the output is a list with four elements:
+#' @return The model simulates virtual tree censuses with coordinates and species identities for each tree. Several summary statistics are calculated from the census data. The summary statistics are calculated for each of the nSteps.out time steps. The model returns a list with the following elements:
 #' \enumerate{
-#'    \item \bold{census} - a dataframe with x,y coordinates and species ID for every tree in the simulated community
-#'    \item \bold{abundance} - a vector with the number of individuals for every species
-#'    \item \bold{species} - a dataframe with species properties: (1) the species ID, (2) the relative abundance in the metacommunity, (3) the mean dispersal distance, (4) the species-specifc conspecific negative density dependence (CNDD)
-#'    \item \bold{summary.stats} - a list with eight elements that includes non-spatial and spatial summary statistics of the simulated community. When nSteps.out == 1 the summary statistics are provided as scalars and vectors and when nStpes.out > 1 as vectors and matrices. The list elements are:
-#' \enumerate{
+#'    \item \bold{census} - a dataframe with x,y coordinates and species ID for every tree in the simulated community. The census represents the last simulated time step and is only provided when nRep = 1 and avg = F.
+#'    \item \bold{abundance} - a vector (or matrix) with the number of individuals for every species. Only provided when nRep = 1 and avg = F.
+#'    \item \bold{species} - a dataframe with species properties: (1) the species ID, (2) the relative abundance in the metacommunity, (3) the mean dispersal distance, (4) the species-specifc conspecific negative density dependence (CNDD), (5) the species-specific recruitment probability without competition. Only provided when nRep = 1 and avg = F.
+#'    \item \bold{generations} - Number of generations for which model output is calculated.
 #'    \item \bold{nSpecies} - Number of species
 #'    \item \bold{Shannon} - Shannon diversity index \eqn{H = - \sum log(pi) * pi}
 #'    \item \bold{Simpson} - Simpson diversity index \eqn{S = 1 - \sum 1/pi^2}, where pi is the relative abundance of species i. This essentially estimates the probability of randomly drawing two individuals from different species (probability of interspecific encounter)
@@ -69,7 +60,7 @@
 #'    \item \bold{PCF} - pair-correlation function. Measure of spatial aggregation of regularity across scales. A value of 1 indicates a random pattern, PCF > 1 indicates aggregation and PCF < 1 regularity (=repulsion of individuals)
 #' }
 #' }
-#' When nRep > 1 or avg = T only the list with the summary statistics is returned.
+#' When nRep > 1 or avg = T only the list with the summary statistics is returned, but no output on trees or single species.
 #'
 #' @examples
 #' #Run CONFETTI with standard parameters
